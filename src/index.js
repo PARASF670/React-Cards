d4726bc0d013749b74c8dadf82d28570a0865a68
parasf <email@youremail.com>
2021-09-06 12:16:59 +0530

    Simple cards

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Cardgroup from './Cardgroup'
+
+ReactDOM.render(
+ <Cardgroup/>
+  ,
+  document.getElementById('root')
+);
+
